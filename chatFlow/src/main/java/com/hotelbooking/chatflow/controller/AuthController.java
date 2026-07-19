8b7369adae61f64c8af341f29acc50c5f5232a2a
ChansereyGit <samnangchanserey@gmail.com>
2026-07-19 22:19:57 +0700

    add again

--- chatFlow/src/main/java/com/hotelbooking/chatflow/controller/AuthController.java
+++ chatFlow/src/main/java/com/hotelbooking/chatflow/controller/AuthController.java
@@ -14,7 +14,7 @@ import reactor.core.publisher.Mono;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/auth")  main
+@RequestMapping("/api/auth")  main hhgods
 @RequiredArgsConstructor
 public class AuthController {
 
